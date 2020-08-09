e7212599e3c8901c2ded8a5075c395f1d650a510
Pedro Blandim <pedro.blandim@seatecnologia.com.br>
2020-08-08 21:39:03 -0300

    implementing function to open all mines in a circular pattern around the first mine opened

--- src/components/Minesweeper/index.tsx
+++ src/components/Minesweeper/index.tsx
@@ -8,8 +8,13 @@ interface ISquare {
   hasMine: Boolean;
   isOpen: Boolean;
   minesAround: number;
+  position: Position;
 }
 
+interface Position {
+  row: number;
+  column: number;
+}
 
 interface GameStatus {
   minesOpened: number;
@@ -23,8 +28,12 @@ interface GameConfigs {
   mines: number;
 }
 
+// TODO transform Time FC in a Class Component and create start and stop function using ref
+// TODO shake animation
 // TODO restart time when the game restarts
+// TODO create the game only at the first click, making the first opened square to be empty
 // TODO do GameOver functions
+// TODO make logic that passes game over animation when user clicks
 // TODO flags counter
 // TODO win function
 
@@ -51,12 +60,39 @@ const gameConfigsOptions:{[key:string]: GameConfigs} = {
 const Minesweeper:React.FC = () => {
 
   const defaultDifficulty = "easy";
+  const defaultSquare     = {hasMine:false, isOpen: false, minesAround: 0, position:{row: 0, column: 0}};
   
   const [gameConfigs, setGameConfigs] = useState<GameConfigs>(gameConfigsOptions[defaultDifficulty]);
   const [gameStatus, setGameStatus]   = useState<GameStatus>({gameOver: false, minesLeft: 0, minesOpened: 0});
 
-  const [squares, setSquares]         = useState<ISquare[][]>([[{hasMine:false, isOpen: false, minesAround: 0}]]);
+  const [squares, setSquares]         = useState<ISquare[][]>([[defaultSquare]]);
   
+
+  useEffect(startGame, []);
+  useEffect(startGame, [gameConfigs]);
+  useEffect(setClickListener, [gameStatus]);
+
+  function startGame(){ // also restarts 
+    createGame();
+    createGameStatus();
+  }  
+
+
+  function createGame(){
+    const {rows, columns} = gameConfigs;
+
+    const squares = createEmptyMatrix<ISquare>(rows, columns, defaultSquare);
+    setPositions(squares);
+    setMines(squares);
+    setNumbers(squares);
+    setSquares(squares);
+
+  }
+
+  const setPositions = (squares: ISquare[][]) => {
+    squares.forEach((row, i) => row.forEach((square, j) => square.position = {row:i, column: j}));
+  }
+
   const setMines = (squares: ISquare[][]) => {
     const {mines} = gameConfigs;
     
@@ -67,8 +103,7 @@ const Minesweeper:React.FC = () => {
   }
 
   const fillWithMinesRandomly = (squares: ISquare[][], minesToPlace: number):  number => {
-    const columns = squares[0].length;
-    const rows    = squares.length;
+    const {rows, columns} = gameConfigs;
     let minesPlaced     = 0;
 
     for(let i = 0; i < rows; i++ ){
@@ -83,20 +118,13 @@ const Minesweeper:React.FC = () => {
   }
 
   const setNumbers = (squares: ISquare[][]) => {
-    const rows    = squares.length;
-    const columns = squares[0].length;
-
-    for(let i = 0; i < rows; i++){
-      for(let j = 0; j < columns; j++){
-        squares[i][j].minesAround = getMinesAroundPlace(i, j, squares);
-      }
-    }
-   
-    
+    squares.forEach(row => row.forEach(square => square.minesAround = getMinesAroundPlace(square.position, squares)));
   }
 
-  const getMinesAroundPlace =  (row: number, column: number, squares: ISquare[][]):number => {
+  const getMinesAroundPlace =  (position: Position, squares: ISquare[][]):number => {
     const {rows:totalRows, columns:totalColumns} = gameConfigs;
+    const {row, column} = position;
+
     let count = 0;
     for(let i = row - 1 ; i <= row + 1; i++){
       
@@ -112,26 +140,7 @@ const Minesweeper:React.FC = () => {
     return count;
   }
 
-  useEffect(startGame, []);
-  useEffect(startGame, [gameConfigs]);
-
-  function startGame(){ // also restarts 
-    createGame();
-    createGameStatus();
-  }  
-
-
-  function createGame(){
-    const {rows, columns} = gameConfigs;
-
-    const squares = createEmptyMatrix<ISquare>(rows, columns, {hasMine: false, isOpen: false, minesAround: 0});
-    setMines(squares);
-    setNumbers(squares);
-    setSquares(squares);
-
-  }
-
-  function createGameStatus(){
+  const createGameStatus = () => {
     const gameStatus: GameStatus ={
       gameOver: false,
       minesLeft: gameConfigs.mines,
@@ -140,34 +149,32 @@ const Minesweeper:React.FC = () => {
     setGameStatus(gameStatus);
   }
 
+  function setClickListener() {
+    // if(!gameOver)
+      // return false;
+
+    // document.body.addEventListener()
+    
+  }
   
-  const sweepMine = (row: number, column: number) => {
-    const {hasMine, isOpen} = squares[row][column];
+  const handleOpening = (row: number, column: number) => {
+    const {hasMine, isOpen, minesAround} = squares[row][column];
     
     if(isOpen)
       return false;
 
+    if(hasMine || minesAround === 0)
+      shakeElement("gameContainer");    
+
     if(hasMine){
       open(row, column);
-      gameOver();  
+      gameOver(row, column);  
     } else {
       openThisAndPlacesAround(row, column);
     }
   }
-  
-  const changeGameDifficulty = (newDifficulty: string) => {
-    let newGameConfigs = gameConfigsOptions[newDifficulty];
-    setGameConfigs(newGameConfigs);
-  }
 
-  function gameOver(){
-    let updatedGameStatus = {...gameStatus};
-    updatedGameStatus.gameOver = true;
-    setGameStatus(updatedGameStatus);
-    console.log("perdeu");
-  }
-
-  function openThisAndPlacesAround(row: number, column: number){
+  const openThisAndPlacesAround = (row: number, column: number) => {
     const {rows: totalRows, columns: totalColumns} = gameConfigs;
     const {hasMine, isOpen, minesAround} = squares[row][column];
 
@@ -208,13 +215,75 @@ const Minesweeper:React.FC = () => {
     }
   }
 
+
   function open(row: number, column: number){
+    let updatedSquares = [...squares];
+    updatedSquares[row][column].isOpen = true;
+    setSquares(updatedSquares);
+  }
+
+  function openPosition(position: Position){
+    const {row, column} = position;
     let updatedSquares = [...squares];
     updatedSquares[row][column].isOpen = true;
     setSquares(updatedSquares);
   }
   
 
+  const gameOver = (row: number, column: number) => {
+    const openedMine = squares[row][column];
+    let updatedGameStatus = {...gameStatus};
+    updatedGameStatus.gameOver = true;
+    setGameStatus(updatedGameStatus);
+    openAllMinesAround(openedMine.position);
+  }
+
+  const openAllMinesAround = (position: Position) => {
+    const allMines = getAllMines();
+
+    // opening the mines in this order is what makes the mines open in a circular pattern
+    sortByDistanceFrom(position, allMines); 
+    openMinesWithDelay(allMines);
+  }
+
+  const getAllMines = (): ISquare[] => {
+    const allMines:ISquare[] = [];
+    const {rows, columns} = gameConfigs;
+
+    for(let i = 0; i < rows; i++){
+      for(let j = 0; j < columns; j++){
+        if(squares[i][j].hasMine)
+          allMines.push(squares[i][j]);
+      }
+    }
+
+    return allMines;
+  }
+
+  const sortByDistanceFrom = (centralPosition: Position, arrayToSort: ISquare[]): ISquare[] => {
+    return arrayToSort.sort((a, b) => {
+      const distanceA = getDistanceBetweenTwoPositions(centralPosition, a.position);
+      const distanceB = getDistanceBetweenTwoPositions(centralPosition, b.position);
+      return distanceB - distanceA;
+    });
+  }
+
+  const openMinesWithDelay = (squaresArray: ISquare[]) => {
+    setTimeout(() =>{
+      if(squaresArray.length === 0)
+        return false;
+      
+      openPosition(squaresArray[squaresArray.length - 1].position);
+      squaresArray.pop();
+      openMinesWithDelay(squaresArray);
+    }, 150 + 50 * (squaresArray.length
+      % 2), squaresArray);
+  }
+  
+  const changeGameDifficulty = (newDifficulty: string) => {
+    let newGameConfigs = gameConfigsOptions[newDifficulty];
+    setGameConfigs(newGameConfigs);
+  }
 
   return (
     <div  className="gameContainer" 
@@ -240,7 +309,7 @@ const Minesweeper:React.FC = () => {
                             opened      = {isOpen} 
                             hasMine     = {hasMine} 
                             minesAround = {minesAround} 
-                            SweepMine   = {sweepMine}  
+                            handleOpening   = {handleOpening}  
                             />;
             })}
           </tr>)
@@ -265,4 +334,28 @@ function createEmptyMatrix<T>(rows: number, columns: number, defaultValue: T): T
     matrix.push(row);
   }
   return matrix;
+}
+
+function getDistanceBetweenTwoPositions(a: Position, b: Position){
+  return Math.sqrt((a.row - b.row)**2 + (a.column - b.column)**2);
+}
+
+function shakeElement(elementaClass: string){
+  const element = document.getElementsByClassName(elementaClass)[0];
+  const movementController = (time: number) => {
+    return Math.sin(time)*2 / time;
+  };
+  let time = 0;
+  doShake(element, movementController(time));
+}
+
+function doShake(element: Element, time: number){
+  // if(time < 1)
+  //   return false;
+  // element.setAttribute("style", "tr")
+  // setTimeout()
+}
+
+function setTranslate(element: HTMLCollectionOf<Element>, x: number){
+
 }
